4cab596f521f3bdf63d8fce8cf33833fd1fc47e2
Bijit-Poudel <149938098+Bijit-Poudel@users.noreply.github.com>
2024-09-03 16:32:56 +0545

    Add files via upload

---
+++ 4logical.js
@@ -0,0 +1,29 @@
+// not operator
+ 
+console.log(true); //true
+console.log(!true); // false
+console.log(false);// false
+console.log(!false);// true
+
+//or operator
+console.log(true || false || true || false); //true
+console.log(true || true || true || false); //true
+console.log(false || false || false || false); //false
+
+/* in or (||)
+output will be true if one is true 
+*/
+
+// AND
+
+console.log(true && false && true && false); //false
+console.log(true && true && true && false); //false
+console.log(false && false && false && false); //false
+console.log(true && true && true && true); //true
+
+/* 
+This is a multi-line comment => alt + shift + a
+&&
+    here output will be true if all are true
+    
+ */
---
+++ comparator.js
@@ -0,0 +1,7 @@
+console.log(10 > 3);//true
+console.log(10 < 3);//false
+console.log(10 >= 10);//true
+console.log(10 >= 10);//true
+console.log(10 <= 10);//true
+console.log(10 === 10);//true
+console.log(10 !== 10);//false
