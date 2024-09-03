597e650cc432b6ebbe561ee42366dcad6d0e3626
Bijit-Poudel <149938098+Bijit-Poudel@users.noreply.github.com>
2024-09-03 16:31:27 +0545

    Add files via upload

---
+++ 1if-else.js
@@ -0,0 +1,10 @@
+if (false){
+    console.log("I am if");
+}
+else{
+    console.log("I am else")
+}
+
+/* if block wil run, if condition is true
+
+else block will run if none of the block gets executed*/
